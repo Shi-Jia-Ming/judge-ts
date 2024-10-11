c10a7629ebfa87507fbece5231fe4494fa86692f
jcyyy3 <1914870249@qq.com>
2024-10-11 16:38:53 +0800

    添加map，应对高并发

--- src/index.ts
+++ src/index.ts
@@ -6,25 +6,27 @@ import {JudgeManager} from "./service/judge/judge.manage";
 import {Web2JudgeMessage} from "./types/client";
 
 const WebSocket = require('ws');
-
 const app =express();
-
 const server = http.createServer(app);
+const {v4 : uuid} = require('uuid')
+const wsInstance = new WebSocket.Server({server})
 
-// 创建 WebSocket 实例
-const wsInstance = new WebSocket.Server({ server });
+// 存储客户端实例
+const clients = new Map();
 
-wsInstance.on('connection', (_ws: any) => {
-  console.log("Client connected!");
-
-  const responseManager: Judge2WebManager = new Judge2WebManager(_ws);
-  responseManager.hello();
-
+wsInstance.on('connection', (_ws:any) => {
+  console.log("Client connected");
+  // 生成client id
+  const clientId = generateClientId(_ws);
+  // 创建管理器实例
+  const responseManager = new Judge2WebManager(_ws);
   const judgeManager: JudgeManager = new JudgeManager(responseManager);
+  // 存储
+  clients.set(clientId, {responseManager, judgeManager});
+  responseManager.hello();
 
   _ws.on('message', (message: string) => {
     console.log('Received message：' + message);
-
     const received: Web2JudgeMessage = JSON.parse(message);
     if (received.type === "task") {
       const isReceived: boolean = judgeManager.receiveTask(received);
@@ -35,10 +37,22 @@ wsInstance.on('connection', (_ws: any) => {
   });
 
   _ws.on('close', () => {
-    console.log("Client closed!");
+    console.log(`Client ${clientId} closed!`);
+    clients.delete(clientId)
   });
-});
+})
 
 server.listen(systemConfig.port, () => {
-  console.log(`the server is start at port ${systemConfig.port}`);
-});
+  console.log(`the server is start at port ${systemConfig.port}`)
+})
+
+
+const generateClientId = (ws:any) => {
+  // 检查 upgradeReq 是否存在
+  if (ws.upgradeReq && ws.upgradeReq.headers) {
+    return ws.upgradeReq.headers['sec-websocket-key'];
+  }
+
+  // 如果 upgradeReq 不存在，使用 uuid 生成唯一标识符
+  return uuid();
+}
