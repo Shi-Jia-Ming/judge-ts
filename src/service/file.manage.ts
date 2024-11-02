b8a347380c73080f694b89647931747071badf4d
Shi-Jia-Ming <sjiaming38@gmail.com>
2024-11-02 19:39:39 +0800

    style: format the code

--- src/service/file.manage.ts
+++ src/service/file.manage.ts
@@ -3,36 +3,36 @@ import { JudgeManager } from "./judge/judge.manage";
 import { Judge2WebManager } from "./response";
 
 type Waiting = {
-    uuid: string;
-    instanceId: number;
+  uuid: string;
+  instanceId: number;
 }
 
 export default class FileManager {
-    private fileList: Array<Waiting> = [];
+  private fileList: Array<Waiting> = [];
 
-    private response: Judge2WebManager;
+  private response: Judge2WebManager;
 
-    constructor(response: Judge2WebManager) {
-        this.response = response;
+  constructor(response: Judge2WebManager) {
+    this.response = response;
+  }
+
+  public requestFile = (uuid: string, instanceId: number) => {
+    if (process.env.RUNNING_LEVEL === "debug") {
+      console.log("[file manager]", "request file", uuid, "for instance", instanceId);
     }
+    this.response.fileSync(uuid);
+    this.fileList.push({ uuid, instanceId });
+  }
 
-    public requestFile = (uuid: string, instanceId: number) => {
-        if (process.env.RUNNING_LEVEL === "debug") {
-            console.log("[file manager]", "request file", uuid, "for instance", instanceId);
-        }
-        this.response.fileSync(uuid);
-        this.fileList.push({ uuid, instanceId });
+  public receiveFile = (file: SyncResponseMessage, judgeManager: JudgeManager) => {
+    const waiting = this.fileList.find((waiting) => waiting.uuid === file.uuid);
+    if (waiting) {
+      judgeManager.saveFile(file, waiting.instanceId);
+      this.fileList.splice(this.fileList.indexOf(waiting), 1);
+    } else {
+      if (process.env.RUNNING_LEVEL === "debug") {
+        console.error("[file manager]", "no judge instance is waiting for this file");
+      }
     }
-
-    public receiveFile = (file: SyncResponseMessage, judgeManager: JudgeManager) => {
-        const waiting = this.fileList.find((waiting) => waiting.uuid === file.uuid);
-        if (waiting) {
-            judgeManager.saveFile(file, waiting.instanceId);
-            this.fileList.splice(this.fileList.indexOf(waiting), 1);
-        } else {
-            if (process.env.RUNNING_LEVEL === "debug") {
-                console.error("[file manager]", "no judge instance is waiting for this file");
-            }
-        }
-    }
+  }
 }
